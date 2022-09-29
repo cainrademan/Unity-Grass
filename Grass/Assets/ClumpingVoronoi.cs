e7c680f2d874938f0df04eaa769d0d77c22e09a7
cainrademan <52975691+cainrademan@users.noreply.github.com>
2022-09-29 14:55:03 +0200

    Cleaned files, Started voronoi

---
+++ Grass/Assets/ClumpingVoronoi.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ClumpingVoronoi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public int width;
+    public int height;
+
+    public Material clumpingVoronoiMat;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        //RenderTexture src = RenderTexture.GetTemporary(width, height, 0, RenderTextureFormat.R8);
+        //RenderTexture dst = RenderTexture.GetTemporary(width, height, 0, RenderTextureFormat.R8);
+        Graphics.Blit(source, destination, clumpingVoronoiMat, 0);
+
+
+        //Graphics.Blit(source,destination, clumpingVoronoiMat);
+
+        //RenderTexture rt = Selection.activeObject as RenderTexture;
+
+        //RenderTexture.active = dst;
+        //Texture2D tex = new Texture2D(dst.width, dst.height, TextureFormat.RGB24, false);
+        //tex.ReadPixels(new Rect(0, 0, dst.width, dst.height), 0, 0);
+        //RenderTexture.active = null;
+
+        //byte[] bytes;
+        //bytes = tex.EncodeToPNG();
+
+        //string path = AssetDatabase.GetAssetPath(dst) + ".png";
+        //System.IO.File.WriteAllBytes(path, bytes);
+        //AssetDatabase.ImportAsset(path);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
